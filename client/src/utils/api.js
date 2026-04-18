79b1b96ee6a092f6966878761189fd8b588272bd
Gouli P Sharath Kumar <goulipsharathkumar@gmail.com>
2026-04-18 19:38:39 +0530

    Update API URL for production

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://hireflow-server-h26o.onrender.com/api'
 });
 
-// Add token to every request automatically
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
   if (token) {
